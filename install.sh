ebfb609096fc940d2b8cc060006f5733fe0de7fa
vilksons <barokahsukawangi@gmail.com>
2025-03-22 02:56:02 +0700

    FIXED BUGS

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/Downloads && curl -l -o workspace https://raw.githubusercontent.com/vilksons/triops/refs/heads/main/Scripts/workspace && \
-        chmod +x workspace && bash ./workspace
